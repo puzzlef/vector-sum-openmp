0c36213406b9892a85b3398112ceb122b2216f81
Subhajit Sahu <wolfram77@gmail.com>
2021-05-19 08:21:02 +0530

    :skunk: run multiple OMP_NUM_THREADS

--- main.sh
+++ main.sh
@@ -10,5 +10,17 @@ git clone https://github.com/puzzlef/$src
 cd $src
 
 # Run
+run() {
 g++ -O3 -fopenmp main.cxx
-stdbuf --output=L ./a.out 2>&1 | tee -a "$out"
+echo "OMP_NUM_THREADS=$OMP_NUM_THREADS" | tee -a "$out"
+stdbuf --output=L ./a.out 2>&1          | tee -a "$out"
+echo ""                                 | tee -a "$out"
+}
+
+export OMP_NUM_THREADS=2  && run
+export OMP_NUM_THREADS=4  && run
+export OMP_NUM_THREADS=8  && run
+export OMP_NUM_THREADS=16 && run
+export OMP_NUM_THREADS=28 && run
+export OMP_NUM_THREADS=32 && run
+export OMP_NUM_THREADS=48 && run
