6ba93d463cbb5efcb21c1c77f7b054bd4f632666
Subhajit Sahu <wolfram77@gmail.com>
2021-11-06 16:01:58 +0530

    :skunk: fix basic errors

--- src/main.hxx
+++ src/main.hxx
@@ -1,5 +1,5 @@
 #pragma once
 #include "_main.hxx"
-#include "multiply.hxx"
-#include "multiplyOpenmp.hxx"
-#include "multiplySeq.hxx"
+#include "sum.hxx"
+#include "sumOpenmp.hxx"
+#include "sumSeq.hxx"
