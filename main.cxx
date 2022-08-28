127ac1d64f5e5176dfbbab82cc3c2dfe5fd77569
Subhajit Sahu <wolfram77@gmail.com>
2022-08-28 19:14:16 +0530

    :mango: update expt

--- main.cxx
+++ main.cxx
@@ -2,65 +2,80 @@
 #include <vector>
 #include <string>
 #include <cstdio>
+#include <omp.h>
 #include "src/main.hxx"
 
 using namespace std;
 
 
 
+
+// You can define datatype with -DTYPE=...
+#ifndef TYPE
 #define TYPE float
+#endif
+// You can define number of threads with -DMAX_THREADS=...
+#ifndef MAX_THREADS
+#define MAX_THREADS 12
+#endif
+
+
 
 
 void runSum(int N, int repeat) {
-  vector<TYPE> x(N);
+  using T = TYPE;
+  vector<T> x(N);
+
+  // Initialize x.
   for (int i=0; i<N; i++) {
-    TYPE n = i+1;
+    T n = i+1;
     x[i] = 1/(n*n);
   }
 
   // Find Σx using a single thread.
-  auto a0 = sumSeq(x, {repeat});
-  printf("[%09.3f ms] [%f] sumSeq\n", a0.time, a0.result);
+  do {
+    T a = T();
+    float t = measureDuration([&]() { a = sumValues(x); }, repeat);
+    printf("[%09.3f ms] [%f] sumSeq\n", t, a);
+  } while (0);
 
-  // Find x*y accelerated using OpenMP.
-  int maxThreads = omp_get_max_threads();
-  for (int i=0;; i++) {
-    bool done = false;
-    int threads = 1<<(i/2) + (i&1)<<(i/4);
-    if (threads>maxThreads) { threads = maxThreads;  done = true; }
-    // Use static schedule.
-    for (int chunkSize=64; chunkSize<=65536; chunkSize*=2) {
-      auto a1 = sumOpenmp(x, {repeat, threads, omp_sched_static, chunkSize});
-      printf("[%09.3f ms] [%f] sumOpenmp [%d threads; schedule static %d]\n", a1.time, a1.result, threads, chunkSize);
-    }
+  // Find Σx accelerated using OpenMP (static schedule).
+  for (int chunkSize=1; chunkSize<=65536; chunkSize*=2) {
+    omp_set_schedule(omp_sched_static, chunkSize); T a = T();
+    float t = measureDuration([&]() { a = sumValuesOmp(x); }, repeat);
+    printf("[%09.3f ms] [%f] sumOmp {sch_kind: static, chunk_size: %d}\n", t, a, chunkSize);
+  }
 
-    // Use dynamic schedule.
-    for (int chunkSize=64; chunkSize<=65536; chunkSize*=2) {
-      auto a2 = sumOpenmp(x, {repeat, threads, omp_sched_dynamic, chunkSize});
-      printf("[%09.3f ms] [%f] sumOpenmp [%d threads; schedule dynamic %d]\n", a2.time, a2.result, threads, chunkSize);
-    }
+  // Find Σx accelerated using OpenMP (dynamic schedule).
+  for (int chunkSize=1; chunkSize<=65536; chunkSize*=2) {
+    omp_set_schedule(omp_sched_dynamic, chunkSize); T a = T();
+    float t = measureDuration([&]() { a = sumValuesOmp(x); }, repeat);
+    printf("[%09.3f ms] [%f] sumOmp {sch_kind: dynamic, chunk_size: %d}\n", t, a, chunkSize);
+  }
 
-    // Use guided schedule.
-    for (int chunkSize=64; chunkSize<=65536; chunkSize*=2) {
-      auto a3 = sumOpenmp(x, {repeat, threads, omp_sched_guided, chunkSize});
-      printf("[%09.3f ms] [%f] sumOpenmp [%d threads; schedule guided %d]\n", a3.time, a3.result, threads, chunkSize);
-    }
+  // Find Σx accelerated using OpenMP (guided schedule).
+  for (int chunkSize=1; chunkSize<=65536; chunkSize*=2) {
+    omp_set_schedule(omp_sched_guided, chunkSize); T a = T();
+    float t = measureDuration([&]() { a = sumValuesOmp(x); }, repeat);
+    printf("[%09.3f ms] [%f] sumOmp {sch_kind: guided, chunk_size: %d}\n", t, a, chunkSize);
+  }
 
-    // Use auto schedule.
-    for (int chunkSize=64; chunkSize<=65536; chunkSize*=2) {
-      auto a4 = sumOpenmp(x, {repeat, threads, omp_sched_auto, chunkSize});
-      printf("[%09.3f ms] [%f] sumOpenmp [%d threads; schedule auto %d]\n", a4.time, a4.result, threads, chunkSize);
-    }
-    if (done) break;
+  // Find Σx accelerated using OpenMP (auto schedule).
+  for (int chunkSize=1; chunkSize<=65536; chunkSize*=2) {
+    omp_set_schedule(omp_sched_auto, chunkSize); T a = T();
+    float t = measureDuration([&]() { a = sumValuesOmp(x); }, repeat);
+    printf("[%09.3f ms] [%f] sumOmp {sch_kind: auto, chunk_size: %d}\n", t, a, chunkSize);
   }
 }
 
 
 int main(int argc, char **argv) {
   int repeat = argc>1? stoi(argv[1]) : 5;
-  for (int n=1000000; n<=1000000000; n*=10) {
-    printf("# Elements %.0e\n", (double) n);
-    runSum(n, repeat);
+  omp_set_num_threads(MAX_THREADS);
+  printf("OMP_NUM_THREADS=%d\n", MAX_THREADS);
+  for (int N=1000000; N<=1000000000; N*=10) {
+    printf("# Elements %.0e\n", (double) N);
+    runSum(N, repeat);
     printf("\n");
   }
   return 0;
